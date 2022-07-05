7d3480edb7da85bee262818b69deb5ed9355e820
Sandace <gsandace@gmail.com>
2022-07-05 15:30:43 +0545

    dependency injectioni added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mockapi_clean_code/injection.dart';
 
 import 'feature/home/presentation/pages/home_page.dart';
 
 void main() {
+  configureDependencies();
   runApp(const MyApp());
 }
 
