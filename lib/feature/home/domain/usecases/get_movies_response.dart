8b996b31b6d45da38075c669ea31e93fef414323
Sandace <gsandace@gmail.com>
2022-07-08 10:56:26 +0545

    feat: usecase of moviesresponse added

---
+++ lib/feature/home/domain/usecases/get_movies_response.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockapi_clean_code/core/failures/failure.dart';
+import 'package:mockapi_clean_code/core/models/no_params.dart';
+import 'package:mockapi_clean_code/core/usecase/usecase.dart';
+import 'package:mockapi_clean_code/feature/home/data/models/MovieResponseModel/movies_response_model.dart';
+import 'package:mockapi_clean_code/feature/home/domain/repository/movies_repository.dart';
+
+class GetMoviesResponseUseCase
+    extends UseCase<ApiFailure, MoviesResponseModel, NoParams> {
+  MoviesRepository moviesRepository;
+  GetMoviesResponseUseCase({required this.moviesRepository});
+
+  @override
+  Future<Either<ApiFailure, MoviesResponseModel>> call(NoParams params) async {
+    return await moviesRepository.getMoviesReponse();
+  }
+}
