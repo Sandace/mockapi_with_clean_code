ae041a4d1edb55063eb4cc59a764f9ce59f83b9d
Sandace <gsandace@gmail.com>
2022-07-08 16:57:53 +0545

    feat:added songs-reponse-usecase

---
+++ lib/feature/home/domain/usecases/get_songs_response.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockapi_clean_code/core/failures/failure.dart';
+import 'package:mockapi_clean_code/core/models/no_params.dart';
+import 'package:mockapi_clean_code/core/usecase/usecase.dart';
+import 'package:mockapi_clean_code/feature/home/data/models/SongsResponse/songs_response_model.dart';
+import 'package:mockapi_clean_code/feature/home/domain/repository/songs_repository.dart';
+
+class GetSongsReponseUseCase
+    extends UseCase<ApiFailure, SongsResponseModel, NoParams> {
+  SongsRepository songsRepository;
+  GetSongsReponseUseCase({required this.songsRepository});
+  @override
+  Future<Either<ApiFailure, SongsResponseModel>> call(NoParams params) async {
+    return await songsRepository.getSongsResponsePageData();
+  }
+}
